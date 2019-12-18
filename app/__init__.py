38ef64e50ff4cc8335ad79aca25397371da773fe
Andre <andre_fagereng@hotmail.com>
2019-12-18 21:59:58 +0100

    draw and upload function

--- app/__init__.py
+++ app/__init__.py
@@ -2,4 +2,9 @@ from flask import Flask
 
 app = Flask(__name__)
 
+UPLOAD_FOLDER = 'C:/Users/Jegern/Desktop/github/digitapp/data/uploaded_img'
+ALLOWED_EXTENSIONS = {'txt', 'pdf', 'png', 'jpg', 'jpeg', 'gif'}
+
 from app import views
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+app.config['IMAGE_UPLOADS'] = UPLOAD_FOLDER
--- app/views.py
+++ app/views.py
@@ -1,9 +1,43 @@
 from app import app
+from flask import render_template, request, redirect
+import os
+import base64
+import time
 
-@app.route("/")
+
+@app.route("/", methods=['GET'])
 def index():
-	return "Hello from views"
+	return render_template("index.html")
 
-@app.route("/about")
-def about():
-	return "About"
+@app.route('/upload-image', methods=['GET','POST'])
+def upload_image():
+
+	if request.method == 'POST':
+		img_b64 = request.values['imageBase64'].split(',')
+		algorithm = request.values['algorithm']
+
+		img_decoded = base64.b64decode(img_b64[1])
+		try:
+			save_image(img_decoded)
+		except:
+			print('An error occured. Read exception')
+
+		return render_template("index.html")
+
+	return render_template("index.html")
+
+
+
+# Saves image to file
+def save_image(image):
+	# Relative path to uploaded images
+	path = 'data/uploaded_img/' 
+	
+	# Converts time to unique filenames
+	filename = str(time.strftime('%Y%m%d-%H%M%S'))
+	with open(path + filename + '.png', 'wb') as file:
+		file.write(image)
+
+
+
+
